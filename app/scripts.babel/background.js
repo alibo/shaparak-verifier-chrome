f1cc446e7208fd3c01793be925b7287db1ae4cfa
Alireza Ahmadi <a.ahmadi94@gmail.com>
2016-07-28 03:36:31 +0430

    Refactor background.js file to match .editorconfig code style

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,21 +1,21 @@
 'use strict';
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.tabs.query({
-        active: true,
-        url: "https://*.shaparak.ir/*"
-    }, tabs => {
-    	const tab = tabs[0];
-        chrome.pageAction.show(tab.id);
-        chrome.pageAction.setIcon({
-        	tabId: tab.id,
-        	path: {
-        		"128": "images/verified_icon_128.png"
-        	}
-        });
-        chrome.pageAction.setTitle({
-        	tabId: tab.id,
-        	title: chrome.i18n.getMessage("verified")
-        })
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  chrome.tabs.query({
+    active: true,
+    url: "https://*.shaparak.ir/*"
+  }, tabs => {
+    const tab = tabs[0];
+    chrome.pageAction.show(tab.id);
+    chrome.pageAction.setIcon({
+      tabId: tab.id,
+      path: {
+        "128": "images/verified_icon_128.png"
+      }
     });
+    chrome.pageAction.setTitle({
+      tabId: tab.id,
+      title: chrome.i18n.getMessage("verified")
+    });
+  });
 });
